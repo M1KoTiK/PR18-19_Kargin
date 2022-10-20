82800d3915650006a35a233a9970f8685d6b6737
M1KoTiK <m1kotik@mail.ru>
2022-10-20 22:23:04 +0700

    added ability which close menu

--- app/src/main/java/com/example/pr18_19kargin/MainActivity.kt
+++ app/src/main/java/com/example/pr18_19kargin/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.pr18_19kargin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var TextChapter : TextView;
+    lateinit var Drawer :DrawerLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        TextChapter = findViewById<TextView>(R.id.MainText)
+        TextChapter = findViewById(R.id.MainText)
+        Drawer = findViewById(R.id.drawer)
     }
     fun ShowToast(text : String)
     {
@@ -21,16 +26,22 @@ class MainActivity : AppCompatActivity() {
     }
     fun onClick(view: View)
     {
+        fun changeTextAndCloseMenu(text:String)
+        {
+            TextChapter.setText(text)
+            Drawer.closeDrawer(GravityCompat.START)
+        }
         when(view.id)
         {
-            R.id.row1 -> TextChapter.setText(R.string.chapter1)
-            R.id.row2 -> TextChapter.setText(R.string.chapter2)
-            R.id.row3 -> TextChapter.setText(R.string.chapter3)
-            R.id.row4 -> TextChapter.setText(R.string.chapter4)
-            R.id.row5 -> TextChapter.setText(R.string.chapter5)
-            R.id.row6 -> TextChapter.setText(R.string.chapter6)
-            R.id.row7 -> TextChapter.setText(R.string.chapter7)
-            R.id.row8 -> TextChapter.setText(R.string.chapter8)
+            R.id.row1 -> changeTextAndCloseMenu(getString(R.string.chapter1))
+            R.id.row2 -> changeTextAndCloseMenu(getString(R.string.chapter2))
+            R.id.row3 -> changeTextAndCloseMenu(getString(R.string.chapter3))
+            R.id.row4 -> changeTextAndCloseMenu(getString(R.string.chapter4))
+            R.id.row5 -> changeTextAndCloseMenu(getString(R.string.chapter5))
+            R.id.row6 -> changeTextAndCloseMenu(getString(R.string.chapter6))
+            R.id.row7 -> changeTextAndCloseMenu(getString(R.string.chapter7))
+            R.id.row8 -> changeTextAndCloseMenu(getString(R.string.chapter8))
+            R.id.content -> Drawer.openDrawer(GravityCompat.START)
         }
     }
 }
